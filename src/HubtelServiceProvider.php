792810eb90ad972fedb020e18c1b50ff7386f7f6
Kwadwo Kyeremeh <kyerematics@gmail.com>
2024-04-03 11:47:40 +0000

    Updated the Service Provider

--- src/HubtelServiceProvider.php
+++ src/HubtelServiceProvider.php
@@ -3,24 +3,23 @@
 namespace NotificationChannels\Hubtel;
 
 use GuzzleHttp\Client;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use NotificationChannels\Hubtel\SMSClients\HubtelSMSClient;
 
-class HubtelServiceProvider extends ServiceProvider
+class HubtelServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Bootstrap the application services.
      */
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__.'/config/hubtel.php' => config_path('hubtel.php'),
-        ]);
+        $this->publishConfig();
 
         $this->app->when(HubtelChannel::class)
             ->needs(HubtelSMSClient::class)
             ->give(function () {
-                $config = config('hubtel.account');
+                $config = config('hubtel-sms.account');
 
                 return new HubtelSMSClient(
                     $config['key'],
@@ -28,5 +27,26 @@ class HubtelServiceProvider extends ServiceProvider
                     new Client
                 );
             });
+    }
+
+    public function register():void
+    {
+        $this->registerConfig();
+    }
+
+    protected function publishConfig(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/hubtel-sms.php' => config_path('hubtel-sms.php')
+        ],'hubtel-sms-config');
+
+    }
+
+    protected function registerConfig():void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/hubtel-sms.php',
+            'hubtel-sms'
+        );
     }
 }
